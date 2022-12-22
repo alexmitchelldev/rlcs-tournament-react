4d83ebb9c457dbf23ed964ae7b6aef3bc02bace7
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-22 21:57:48 +0000

    Import useState, add createTeams function & pass to RunTournament compo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import RunTournament from "./components/RunTournament";
 import Teams from "./components/Teams";
 import TournamentResult from "./components/TournamentResult";
+import { useState } from "react";
 
 function App() {
   const players = [
@@ -19,12 +20,38 @@ function App() {
     { name: "Chronic" },
     { name: "Archie" },
   ];
-  const teams = [];
+  const [teams, setTeams] = useState([
+    
+  ]);
+
+  const createTeams = () => {
+    let usedPlayers = [];
+    let teams = [];
+
+    for (let i = 0; i < 4; i++) {
+        let team = [];
+        for (let j = 0; j < 3; j++) {
+            let randomIndex = Math.floor(Math.random() * players.length);
+
+            while (usedPlayers.includes(players[randomIndex])) {
+                randomIndex = Math.floor(Math.random() * players.length);
+            }
+
+            usedPlayers.push(players[randomIndex]);
+            team.push(players[randomIndex]);
+        }
+        teams.push(team);
+    }
+
+    setTeams(teams);
+    console.log(teams);
+  };
+
   return (
     <>
       <div>
         <Header />
-        <RunTournament />
+        <RunTournament createTeams={createTeams}/>
         <Teams />
         <TournamentResult />
       </div>
