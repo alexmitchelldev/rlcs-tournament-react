dd48f6ea71d38fb5dbd1436a7b6d2a4641020e65
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-22 21:58:01 +0000

    Add props to RunTournament

--- src/components/RunTournament.js
+++ src/components/RunTournament.js
@@ -1,4 +1,4 @@
-const RunTournament = () => {
+const RunTournament = (props) => {
     return (
         <>
             <label for="number-of_teams">Select number of teams:</label>
@@ -7,7 +7,7 @@ const RunTournament = () => {
                 {/* <option value="8">8</option> */}
                 {/* <option value="16">16</option> */}
             </select>
-            <button>Create Teams</button>
+            <button onClick={props.createTeams}>Create Teams</button>
             <br></br>
             {/* <button>
                 Run tournament
