9246d3054e408c03baea6376f542d1cc6f73a581
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-22 20:50:29 +0000

    Temporarily hide result component

--- src/components/TournamentResult.js
+++ src/components/TournamentResult.js
@@ -1,15 +1,18 @@
 const TournamentResult = () => {
-    return (
-        <>
-            <ul>
-                <li>1st - </li>
-                <li>2nd - </li>
-                <li>3rd/4th - </li>
-                <li>5-8th - </li>
-                <li>9-16th - </li>
-            </ul>
-        </>
-    )
+  const displayResult = false;
+  return (
+    <>
+      {displayResult ? (
+        <ul>
+          <li>1st - </li>
+          <li>2nd - </li>
+          <li>3rd/4th - </li>
+          <li>5-8th - </li>
+          <li>9-16th - </li>
+        </ul>
+      ) : null}
+    </>
+  );
 };
 
 export default TournamentResult;
