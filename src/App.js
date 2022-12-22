9457bd3e07cda3d617ba36dd1fb7119c39844f7d
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-22 20:49:57 +0000

    Add players array, import Teams component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './index.css';
-import Header from './components/Header';
-import RunTournament  from './components/RunTournament';
-import TournamentResult from './components/TournamentResult';
+import "./index.css";
+import Header from "./components/Header";
+import RunTournament from "./components/RunTournament";
+import Teams from "./components/Teams";
+import TournamentResult from "./components/TournamentResult";
 
 function App() {
+  const players = [
+    { name: "Aztral" },
+    { name: "ApparentlyJack" },
+    { name: "Joreuz" },
+    { name: "Joyo" },
+    { name: "Rise" },
+    { name: "Vatira" },
+    { name: "Jstn" },
+    { name: "GarrettG" },
+    { name: "Squishy" },
+    { name: "Noly" },
+    { name: "Chronic" },
+    { name: "Archie" },
+  ];
+  const teams = [];
   return (
     <>
       <div>
         <Header />
         <RunTournament />
+        <Teams />
         <TournamentResult />
       </div>
     </>
