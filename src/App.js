4a363d0a269ba16d8f58d47497dd89530ac8e478
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-22 20:23:50 +0000

    Add TournamentResult component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './index.css';
 import Header from './components/Header';
 import RunTournament  from './components/RunTournament';
+import TournamentResult from './components/TournamentResult';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Header />
         <RunTournament />
+        <TournamentResult />
       </div>
     </>
   );
---
+++ src/components/TournamentResult.js
@@ -0,0 +1,15 @@
+const TournamentResult = () => {
+    return (
+        <>
+            <ul>
+                <li>1st - </li>
+                <li>2nd - </li>
+                <li>3rd/4th - </li>
+                <li>5-8th - </li>
+                <li>9-16th - </li>
+            </ul>
+        </>
+    )
+};
+
+export default TournamentResult;
