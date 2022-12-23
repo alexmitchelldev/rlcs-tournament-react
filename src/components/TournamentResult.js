07521ef0fbe789120028fc21c3adf21c6dc8cbc1
mitchting <amitchell@ctatax.uk.com>
2022-12-23 09:39:36 +0000

    Add logic to display tournament result

--- src/components/TournamentResult.js
+++ src/components/TournamentResult.js
@@ -1,15 +1,32 @@
-const TournamentResult = () => {
-  const displayResult = false;
+const TournamentResult = (props) => {
+  const indexMap = {
+    0: "1st: ",
+    1: "2nd: ",
+    2: "3rd/4th: ",
+    3: "3rd/4th: ",
+  };
   return (
     <>
-      {displayResult ? (
+      <button onClick={props.run}>Run Tournament</button>
+      {props.displayResults ? (
+        <>
+        <h3>RESULTS!!</h3>
         <ul>
-          <li>1st - </li>
-          <li>2nd - </li>
-          <li>3rd/4th - </li>
-          <li>5-8th - </li>
-          <li>9-16th - </li>
+          {props.results.map((team, index) => {
+            let resultString = "";
+            team.forEach((player, index) => {
+              index !== 2
+                ? (resultString += `${player.name}, `)
+                : (resultString += `${player.name}`);
+            });
+            return (
+              <li key={index}>
+                {indexMap[index]} {resultString}
+              </li>
+            );
+          })}
         </ul>
+        </>
       ) : null}
     </>
   );
