508435725605b35b5f2bf97eabebb86bc7569e68
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-25 22:53:44 +0000

    Remove class

--- src/components/SetupTournament.js
+++ src/components/SetupTournament.js
@@ -9,7 +9,6 @@ const SetupTournament = (props) => {
           props.createTeams();
         }}
         id="setup-tournament"
-        className="w-full max-w-lg"
       >
         <div className="flex flex-wrap -mx-3 mb-2">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
