fa17bbe5ea0d190606901a3e8d90f3a1ec5ad68f
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-25 20:58:44 +0000

    Display Teams in table

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,23 +1,51 @@
 const Teams = (props) => {
   return (
     <>
-      <h2 className="text-3xl font-bold mb-8">Teams</h2>
-      <div className="text-left">
-        <ol>
-          {props.teams.map((team, index) => {
-            let teamString = "";
-            team.forEach((player, index) => {
-              index !== 2
-                ? (teamString += `${player.name}, `)
-                : (teamString += `${player.name}`);
-            });
-            return (
-              <li key={index}>
-                Team{index + 1}: {teamString}
-              </li>
-            );
-          })}
-        </ol>
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full">
+                <thead class="bg-white border-b">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      {props.teamSize === 1 ? 'Player #' : 'Team #'}
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-center"
+                    >
+                      {props.teamSize === 1 ? 'Player' : 'Players'}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {props.teams.map((team, index) => {
+                    let players = team.map((player) => {
+                      return (
+                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {player.name}
+                        </td>
+                      );
+                    });
+
+                    return (
+                      <tr class="bg-gray-100 border-b">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                          {index + 1}
+                        </td>
+                        {players}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
