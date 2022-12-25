d4ce78f2ee55406d520db780c2f0786c8146c5d5
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-25 21:04:27 +0000

    Add position styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 const header = () => {
     return (
       <>
-        <h1 className="text-5xl font-bold mt-0 mb-6">RLCS Tournament Simulator</h1>
-        <h2 className="text-3xl font-bold mb-8">How to use</h2>
+      <div className="text-center">
+      <h1 className="text-5xl font-bold mt-0 mb-6">RLCS Tournament Simulator</h1>
+        <h2 className="text-3xl font-bold mb-8 text-left">How to use</h2>
         <ol className="list-decimal text-left">
             <li>Select the number of teams you would like to be in the tournament.</li>
             <li>Click 'Create Teams' to allocate the players into teams.</li>
             <li>Click 'Run Tournament' to run the tournament simulation and see the results.</li>
         </ol>
-        <p></p>
-        <br></br>
+      </div>
+        
       </>
     );
 };
