65542afe0c746ebaa0b00a5246e76af903b419c1
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-25 13:34:36 +0000

    Add styling to header and ol

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,18 +1,24 @@
 const Teams = (props) => {
   return (
     <>
-      <h2>Teams</h2>
-      <ol>
-        {props.teams.map((team, index) => {
-          let teamString = "";
-          team.forEach((player, index) => {
-            index !== 2
-              ? (teamString += `${player.name}, `)
-              : (teamString += `${player.name}`);
-          });
-          return <li key={index}>Team{index + 1}: {teamString}</li>;
-        })}
-      </ol>
+      <h2 className="text-3xl font-bold mb-8">Teams</h2>
+      <div className="text-left">
+        <ol>
+          {props.teams.map((team, index) => {
+            let teamString = "";
+            team.forEach((player, index) => {
+              index !== 2
+                ? (teamString += `${player.name}, `)
+                : (teamString += `${player.name}`);
+            });
+            return (
+              <li key={index}>
+                Team{index + 1}: {teamString}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     </>
   );
 };
