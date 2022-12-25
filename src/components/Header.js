0af4cae8431f6721fffbb7314ec975824b0e2e22
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-25 13:34:19 +0000

    Switch to h2

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const header = () => {
     return (
       <>
         <h1 className="text-5xl font-bold mt-0 mb-6">RLCS Tournament Simulator</h1>
-        <h3 className="text-3xl font-bold mb-8">How to use</h3>
+        <h2 className="text-3xl font-bold mb-8">How to use</h2>
         <ol className="list-decimal text-left">
             <li>Select the number of teams you would like to be in the tournament.</li>
             <li>Click 'Create Teams' to allocate the players into teams.</li>
