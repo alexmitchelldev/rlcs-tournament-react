d2fc4233779e08691766d094014e57ee85acb1d4
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-29 14:03:38 +0000

    Remove how to use header, place instructions in ul element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 const header = () => {
-    return (
-      <>
+  return (
+    <>
       <div className="text-center mb-5">
-      <h1 className="text-5xl font-bold mt-0 mb-6">RLCS Tournament Simulator</h1>
-        <h2 className="text-3xl font-bold mb-8 text-left">How to use</h2>
-        <ol className="list-decimal text-left">
-            <li>Select the number of teams you would like to be in the tournament.</li>
-            <li>Click 'Create Teams' to allocate the players into teams.</li>
-            <li>Click 'Run Tournament' to run the tournament simulation and see the results.</li>
-        </ol>
+        <h1 className="text-5xl font-bold mt-0 mb-6">
+          RLCS Tournament Simulator
+        </h1>
+        <ul class="list-none">
+          <li>Select the tournament type and number of teams</li>
+          <li>
+            Click 'Create Teams' to randomly allocate players into teams
+          </li>
+          <li>
+            Click 'Run Tournament' to run the tournament simulation and see
+            the results
+          </li>
+        </ul>
       </div>
-        
-      </>
-    );
+    </>
+  );
 };
 
 export default header;
