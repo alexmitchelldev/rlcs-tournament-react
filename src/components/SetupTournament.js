f76a0f13c98c2c0ba6ba655b2d02148c2a255920
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-29 14:34:29 +0000

    Center form field labels

--- src/components/SetupTournament.js
+++ src/components/SetupTournament.js
@@ -10,7 +10,7 @@ const SetupTournament = (props) => {
         }}
         id="setup-tournament"
       >
-        <div className="flex flex-wrap -mx-3 mb-2">
+        <div className="flex flex-wrap -mx-3 mb-2 text-center">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
