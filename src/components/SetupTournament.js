fd774b7715d31d63257aa572175185fc51741627
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-29 14:04:02 +0000

    Remove w3.org div

--- src/components/SetupTournament.js
+++ src/components/SetupTournament.js
@@ -5,7 +5,7 @@ const SetupTournament = (props) => {
         onSubmit={(e) => {
           //prevent page refresh
           e.preventDefault();
-          
+
           props.createTeams();
         }}
         id="setup-tournament"
@@ -63,30 +63,16 @@ const SetupTournament = (props) => {
                 <option value="8">8</option>
                 <option value="4">4</option>
               </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
             </div>
           </div>
         </div>
-        <div className="md:items-center">
-          <div className="w-full"></div>
-          <div className="w-full">
-            <button
-              className="shadow bg-white hover:bg-blue-400 hover:text-white border border-blue-500 focus:shadow-outline focus:outline-none text-blue-500 font-bold py-2 px-4 rounded w-full"
-              form="setup-tournament"
-              value="Submit"
-            >
-              Create Teams
-            </button>
-          </div>
-        </div>
+        <button
+          className="shadow bg-white hover:bg-blue-400 hover:text-white border border-blue-500 focus:shadow-outline focus:outline-none text-blue-500 font-bold py-2 px-4 rounded w-full"
+          form="setup-tournament"
+          value="Submit"
+        >
+          Create Teams
+        </button>
       </form>
     </>
   );
