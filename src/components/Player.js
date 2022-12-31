2cbc5d50ff4846e44cafa881193ed2fe52cb483e
Alex Mitchell <amitchell@ctatax.uk.com>
2022-12-31 13:15:26 +0000

    Remove players array from Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,24 +1,3 @@
-let players = [
-  { name: "Aztral" }, { name: "ApparentlyJack" }, { name: "Joreuz" },
-  { name: "Joyo" }, { name: "rise." }, { name: "vatira." },
-  { name: "jstn." }, { name: "GarrettG" }, { name: "SquishyMuffinz" },
-  { name: "noly" }, { name: "Chronic" }, { name: "Archie" },
-  { name: "caard" }, { name: "yanxnz" }, { name: "CaioTG1" },
-  { name: "M0nkey M00n" }, { name: "Extra" }, { name: "Seikoo" },
-  { name: "Chicago" }, { name: "Atomic" }, { name: "Jknaps" },
-  { name: "Sypical" }, { name: "Firstkiller" }, { name: "AYYJAYY" },
-  { name: "trk511" }, { name: "oKhaliD" }, { name: "Ahmad" },
-  { name: "Deevo" }, { name: "crr" }, { name: "Deevo" },
-  { name: "DORITO" }, { name: "ApparentlyJack" }, { name: "Daniel" },
-  { name: "torment" }, { name: "comm" }, { name: "BeastMode" },
-  { name: "Arsenal" }, { name: "Retals" }, { name: "Itachi" },
-  { name: "Faith" }, { name: "ams" }, { name: "RelatingWave" },
-  { name: "Zez0nix" }, { name: "M7sN" }, { name: "ExoTiiK" },
-  { name: "shad" }, { name: "Rw9." }, { name: "ajg." },
-  { name: "Metsanauris" }, { name: "Reysbull" }, { name: "LionBlaze" },
-  { name: "Scrub Killa"} 
-];
-
 class Player{
   constructor(name) {
     this.name = name;
@@ -35,11 +14,3 @@ class Player{
     return (this.attack + this.defence) / 2; 
   }
 }
-
-players = players.map(player => {
-  return new Player(player.name);
-});
-
-
-
-export { players };
